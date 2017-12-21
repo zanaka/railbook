3fb3961e3740d95711272f3fc82a99bf64403d41
zanaka <e155752@ie.u-ryukyu.ac.jp>
2017-12-22 01:16:32 +0900

    helloコントローラーにindexとviewメソッドの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'hello/index', to: 'hello#index'
+  # 両者が一致している場合は，toオプションを省略できる
+  get 'hello/view'
 end
