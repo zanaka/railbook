c556ff4ff795136965406593d3619bab2881a74a
zanaka <e155752@ie.u-ryukyu.ac.jp>
2017-12-28 00:30:59 +0900

    bookテーブルの情報を全て取得するlistメソッドの追加

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -6,4 +6,10 @@ class HelloController < ApplicationController
   def view
     @msg = 'こんにちは，世界！'
   end
+
+  def list
+    # Bookモデルのレコードの全てをインスタンス変数@booksに格納
+    # @booksにはBookオブジェクトの配列が渡される
+    @books = Book.all
+  end
 end
