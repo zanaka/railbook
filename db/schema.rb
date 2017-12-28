66cc825777e5424363edfa1fe2b52f449a7c942e
zanaka <e155752@ie.u-ryukyu.ac.jp>
2017-12-29 00:16:59 +0900

    bookモデル追加によるフィクスチャファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227152053) do
+ActiveRecord::Schema.define(version: 20171228150511) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
