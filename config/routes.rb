447f5983193158d51ee204a40a804c6c13f80908
zanaka <e155752@ie.u-ryukyu.ac.jp>
2017-12-29 00:15:38 +0900

    bookモデル追加によるルーティングの変更(resource)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resourcesメソッドによってbooksのリソースに対する標準的な操作がまとめて定義可能
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello/index', to: 'hello#index'
   # 両者が一致している場合は，toオプションを省略できる
