399277647940bf440722011d35336bdb44898bbd
zanaka <e155752@ie.u-ryukyu.ac.jp>
2018-01-09 17:02:33 +0900

    collection_selectメソッドの追加

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -5,3 +5,10 @@ end
 def select
   @book = Book.new(publish: '技術評論社')
 end
+
+def col_select
+  #フォームの元となるモデルを準備
+  @book = Book.new(publish: '技術評論社')
+  #選択オプションの情報を取得
+  @books = Book.select(:publish).distinct
+end
